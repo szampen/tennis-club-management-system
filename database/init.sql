085bd237bc77b462a87d73083f12ddf8fca221e1
szampen <maciejduda1@icloud.com>
2025-12-23 19:38:54 +0100

    repositories creation database docker init jbcrypt for passwords dependency added

---
+++ database/init.sql
@@ -0,0 +1,50 @@
+USE tennis_db;
+
+CREATE TABLE IF NOT EXISTS users(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20),
+    user_type ENUM('ADMIN', 'PLAYER') NOT NULL,
+    ranking_points INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)CHARACTER SET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS courts (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    court_number INT NOT NULL,
+    surface_type ENUM('CLAY', 'GRASS', 'HARD', 'CARPET') NOT NULL,
+    has_roof BOOLEAN DEFAULT FALSE,
+    location VARCHAR(255) NOT NULL,
+    image_url VARCHAR(500),
+    available_for_reservations BOOLEAN DEFAULT TRUE,
+    price_per_hour DECIMAL(10,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) CHARACTER SET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS reservations (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    court_id BIGINT NOT NULL,
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    status ENUM('ACTIVE','CANCELLED', 'COMPLETED') NOT NULL,
+    version BIGINT DEFAULT 0, --TODO
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (court_id) REFERENCES courts(id) ON DELETE CASCADE
+) CHARACTER SET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS payments(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    reservation_id BIGINT NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    status ENUM('PENDING', 'COMPLETED', 'FAILED', 'REFUNDED') NOT NULL,
+    payment_date DATETIME,
+    transaction_id VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (reservation_id) REFERENCES reservations(id) ON DELETE CASCADE
+) CHARACTER SET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ src/main/java/com/tennis/repository/CourtRepository.java
@@ -0,0 +1,4 @@
+package com.tennis.repository;
+
+public class CourtRepository {
+}
