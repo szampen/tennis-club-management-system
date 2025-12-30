e1bf66e8bc72c217a8dadadae697a95911af5e7d
szampen <maciejduda1@icloud.com>
2025-12-30 18:42:51 +0100

    Initial implementation of CourtMapper

--- src/main/java/com/tennis/mapper/CourtMapper.java
+++ src/main/java/com/tennis/mapper/CourtMapper.java
@@ -1,18 +1,148 @@
 package com.tennis.mapper;
 
 import com.tennis.domain.Court;
+import com.tennis.domain.SurfaceType;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
+//TODO: use of identity map
 public class CourtMapper implements DataMapper<Court>{
 
     @Override
-    public Long insert(Court court, Connection connection){return null;}
+    public Long insert(Court court, Connection connection) throws SQLException {
+        String sql = "INSERT INTO courts (name, court_number, surface_type, has_roof, location, image_url,available_for_reservations, price_per_hour) VALUES (?,?,?,?,?,?,?,?)".formatted();
+
+        PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+        statement.setString(1,court.getName());
+        statement.setInt(2,court.getCourtNumber());
+        statement.setString(3,court.getSurfaceType().name());
+        statement.setBoolean(4,court.hasRoof());
+        statement.setString(5, court.getLocation());
+        statement.setString(6, court.getImageUrl());
+        statement.setBoolean(7,court.isAvailableForReservations());
+        statement.setDouble(8,court.getPricePerHour());
+
+        statement.executeUpdate();
+
+        ResultSet generatedKeys = statement.getGeneratedKeys();
+        if(generatedKeys.next()){
+            Long id = generatedKeys.getLong(1);
+            court.setId(id);
+            return id;
+        }
+
+        throw new SQLException("Error fetching court id.");
+    }
 
     @Override
-    public void update(Court court, Connection connection){}
+    public void update(Court court, Connection connection) throws SQLException{
+        String sql = "UPDATE courts SET name = ?, court_number = ?, surface_type = ?, has_roof = ?, location = ?, image_url = ?, available_for_reservations = ?, price_per_hour = ? WHERE id = ?".formatted();
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1,court.getName());
+        statement.setInt(2,court.getCourtNumber());
+        statement.setString(3,court.getSurfaceType().name());
+        statement.setBoolean(4,court.hasRoof());
+        statement.setString(5, court.getLocation());
+        statement.setString(6, court.getImageUrl());
+        statement.setBoolean(7,court.isAvailableForReservations());
+        statement.setDouble(8,court.getPricePerHour());
+        statement.setLong(9,court.getId());
+
+        statement.executeUpdate();
+    }
 
     @Override
-    public void delete(Court court, Connection connection){}
+    public void delete(Court court, Connection connection) throws SQLException{
+        String sql = "DELETE FROM courts WHERE id = ?".formatted();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setLong(1,court.getId());
+        statement.executeUpdate();
+    }
+
+    private Court mapResultSetToCourt(ResultSet rs) throws SQLException{
+        Court court = new Court();
+        court.setId(rs.getLong("id"));
+        court.setName(rs.getString("name"));
+        court.setCourtNumber(rs.getInt("court_number"));
+        court.setHasRoof(rs.getBoolean("has_roof"));
+        court.setLocation(rs.getString("location"));
+        court.setImageUrl(rs.getString("image_url"));
+        court.setAvailableForReservations(rs.getBoolean("available_for_reservations"));
+        court.setPricePerHour(rs.getDouble("price_per_hour"));
+        court.setSurfaceType(SurfaceType.valueOf(rs.getString("surface_type")));
+        return court;
+    }
+
+    public Court findById(Long id, Connection connection) throws SQLException{
+        String sql = "SELECT * FROM courts WHERE id = ?".formatted();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setLong(1,id);
+
+        ResultSet rs = statement.executeQuery();
+
+        if(rs.next()){
+            Court court = mapResultSetToCourt(rs);
+            return court;
+        }
+        return null;
+    }
+
+    public List<Court> findAllCourts(Connection connection) throws SQLException{
+        List<Court> courts = new ArrayList<>();
+        String sql = "SELECT * FROM courts".formatted();
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+
+        while(rs.next()){
+            courts.add(mapResultSetToCourt(rs));
+        }
+
+        return courts;
+    }
+
+    public List<Court> findAvailableForReservations(Connection connection) throws SQLException{
+        List<Court> courts = new ArrayList<>();
+        String sql = "SELECT * FROM courts WHERE available_for_reservations = true".formatted();
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+
+        while(rs.next()){
+            courts.add(mapResultSetToCourt(rs));
+        }
+
+        return courts;
+    }
+
+    public List<Court> findBySurface(SurfaceType surfaceType, Connection connection) throws SQLException{
+        List<Court> courts = new ArrayList<>();
+        String sql = "SELECT * FROM courts WHERE surface_type = ?".formatted();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, surfaceType.name());
+        ResultSet rs = statement.executeQuery();
+
+        while(rs.next()){
+            courts.add(mapResultSetToCourt(rs));
+        }
+
+        return courts;
+    }
+
+    public List<Court> findByRoof(Boolean hasRoof, Connection connection) throws SQLException{
+        List<Court> courts = new ArrayList<>();
+        String sql = "SELECT * FROM courts WHERE has_roof = ?".formatted();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setBoolean(1,hasRoof);
+        ResultSet rs = statement.executeQuery();
+
+        while(rs.next()){
+            courts.add(mapResultSetToCourt(rs));
+        }
+
+        return courts;
+    }
+
 
 }
